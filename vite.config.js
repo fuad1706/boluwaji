6ca2ab669eb0e557d8415b091c6eda5843336d66
fuad <fuadshorlarxwix@gmail.com>
2025-07-25 09:51:25 +0100

    page reload

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react"; // If you're using React
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  base: "/", // Ensures proper asset paths
 });
